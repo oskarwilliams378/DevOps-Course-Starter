a7ead87f8973d99674037e6bd212392542a1850f
Oskar Williams <oskarwilliams378@gmail.com>
2020-05-29 14:41:43 +0100

    feat: Add form to add item

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from session_items import get_items, add_item
 import session_items as session
 
 app = Flask(__name__)
@@ -6,7 +7,18 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template('index.html', items=items)
+
+@app.route('/AddItem')
+def addItem():
+    return render_template('addItem.html')
+
+@app.route('/AddItem/Save', methods=['POST'])
+def addItemSave():
+    title = request.form.get('title')
+    add_item(title)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
