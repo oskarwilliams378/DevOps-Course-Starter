fd3d57ed424a59b6397c09bf05445293dc023250
Oskar Williams <oskarwilliams378@gmail.com>
2020-07-21 15:50:35 +0100

    test: Add extra integration test

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,9 +4,18 @@ import pytest
 class TestApp:
 
     @staticmethod
-    def test_index_page_mocking_requests(monkeypatch_fixture, client):
+    def test_index_page(monkeypatch_fixture, client):
         response = client.get('/')
         assert response.status_code == 200
         response_data = response.data.decode("utf-8")
         assert "name 1" in response_data
         assert "desc 4" in response_data
+
+    @staticmethod
+    def test_add_item_page(monkeypatch_fixture, client):
+        response = client.get('/AddItem')
+        assert response.status_code == 200
+        response_data = response.data.decode("utf-8")
+        assert "Title" in response_data
+        assert "Description" in response_data
+        assert "Due date" in response_data
