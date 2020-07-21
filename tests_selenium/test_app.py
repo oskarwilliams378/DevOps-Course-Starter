1e378593b8e7cddedde1d3712c195a6fe58f6227
Oskar Williams <oskarwilliams378@gmail.com>
2020-07-21 15:48:32 +0100

    feat: Complete selenium tests

--- tests_selenium/test_app.py
+++ tests_selenium/test_app.py
@@ -7,3 +7,36 @@ class TestApp:
     def test_index_page(driver, test_app):
         driver.get('http://localhost:5000/')
         assert driver.title == 'To-Do App'
+
+    @staticmethod
+    def test_task_journey(driver, test_app):
+        driver.get('http://localhost:5000/')
+
+        add_another_button = driver.find_element_by_link_text('Add another item')
+        add_another_button.click()
+
+        title = driver.find_element_by_name("title")
+        description = driver.find_element_by_name("description")
+        due_date = driver.find_element_by_name("due_date")
+
+        title.send_keys("Test Title")
+        description.send_keys("Test Description")
+        due_date.send_keys("07/29/2020")
+        save_button = driver.find_element_by_xpath('//button[normalize-space()="Save"]')
+        save_button.click()
+
+        start_task_button = driver.find_element_by_xpath(
+            '//b[normalize-space()="Test Title"]/../button[normalize-space()="Start task"]')
+        start_task_button.click()
+
+        complete_task_button = driver.find_element_by_xpath(
+            '//b[normalize-space()="Test Title"]/../button[normalize-space()="Complete task"]')
+        complete_task_button.click()
+
+        restart_task_button = driver.find_element_by_xpath(
+            '//b[normalize-space()="Test Title"]/../button[normalize-space()="Restart task"]')
+        restart_task_button.click()
+
+        second_complete_task_button = driver.find_element_by_xpath(
+            '//b[normalize-space()="Test Title"]/../button[normalize-space()="Complete task"]')
+        assert second_complete_task_button
