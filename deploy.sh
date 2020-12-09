7e2a4f1c183651e94150c0fdca6dab8d351904b0
Oskar Williams <oskarwilliams378@gmail.com>
2020-12-09 15:40:17 +0000

    chore: Update heroku login again

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker login --username=_ --password="$(heroku auth:token)" registry.heroku.com
+echo "$HEROKU_API_KEY" | docker login --username=_ --password-stdin registry.heroku.com
 docker pull oskwil/todo-app
 docker tag oskwil/todo-app registry.heroku.com/oskwil-todo-app/web
 docker push registry.heroku.com/oskwil-todo-app/web
