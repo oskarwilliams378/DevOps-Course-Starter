7d463e694f5fa71c0d00cc26a9a7bcb657c5fce4
Oskar Williams <oskarwilliams378@gmail.com>
2020-12-09 15:52:51 +0000

    feat: Update deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ echo "$HEROKU_API_KEY" | docker login --username=_ --password-stdin registry.her
 docker pull oskwil/todo-app
 docker tag oskwil/todo-app registry.heroku.com/oskwil-todo-app/web
 docker push registry.heroku.com/oskwil-todo-app/web
-heroku container:release web
+heroku container:release web -a oskwil-todo-app
